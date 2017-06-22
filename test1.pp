6895007ab3a50ec198fd2f7d33495575c4fadc63
shilpa kashyap <shilpa kashyap>
2017-06-22 19:24:43 +0530

    --added

--- test1.pp
+++ test1.pp
@@ -1,6 +1,7 @@
 notify { 'notifytest':
 
-message =>"this is first test"
-message =>"this is second test"
-message =>"this is third test"
+message =>"this is first test",
+message =>"this is second test",
+message =>"this is third test",
+message =>"this is forth test"
  } 
